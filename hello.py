fa98dad715baa102973359c69d54a6dddae08bb1
hirokatsuIino <letter6212@gmail.com>
2018-07-04 17:57:47 +0900

    modef files

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-print("Hello-itmono-World")
+# print("Hello-itmono-World")
+
+import os
+from bottle import route, run
+
+@route("/")
+def hello_world():
+        return "Hello World!"
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
